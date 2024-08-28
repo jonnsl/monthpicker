8dfeb2b836b09a88a8c1a9834d593ee85992a4c8
Jonnathan Soares <jonnsl@hotmail.com>
2024-08-28 17:50:26 -0300

    Refactor to function component

--- src/MonthPicker.tsx
+++ src/MonthPicker.tsx
@@ -1,6 +1,6 @@
 
 import './MonthPicker.css'
-import React, { Component, KeyboardEvent, MouseEvent } from 'react'
+import React, { KeyboardEvent, MouseEvent, useState } from 'react'
 import Header from './Header'
 import Months from './Months'
 
@@ -21,209 +21,102 @@ type MonthPickerProps = {
   onChange: (value: YearMonth | null) => void;
 }
 
-type MonthPickerState = {
-  open: boolean;
-  year: number;
-}
+const defaultCurrentYear = (new Date()).getFullYear()
+const defaultMin = { year: 1970, month: 0 }
+const defaultMax = { year: 2037, month: 11 }
 
-export default class MonthPicker extends Component<MonthPickerProps, MonthPickerState> {
+export default function MonthPicker(props: MonthPickerProps) {
+  const { value, name, className = '', isClearable = false, onChange, min = defaultMin, max = defaultMax, currentYear = defaultCurrentYear } = props
+  const [open, setOpen] = useState(false)
+  const [year, setYear] = useState(2021)
 
-  constructor (props: MonthPickerProps) {
-    super(props)
-    this.state = {
-      open: false,
-      year: 2021,
-    }
-    this.goToPrevYear = this.goToPrevYear.bind(this)
-    this.goToNextYear = this.goToNextYear.bind(this)
-    this.goToCurrentYear = this.goToCurrentYear.bind(this)
-    this.showCalendar = this.showCalendar.bind(this)
-    this.hideCalendar = this.hideCalendar.bind(this)
+  const showCalendar = (): void  => {
+    setOpen(true)
   }
 
-  static getDerivedStateFromProps (props: MonthPickerProps, state: MonthPickerState) {
-    if (state.open) {
-      return null
-    }
-    if (!props.value) {
-      return { year: 2021 }
-    }
-
-    let year = props.value.year
-    year = Math.min(year, props.max ? props.max.year : 2037)
-    year = Math.max(year, props.min ? props.min.year : 1970)
-    return { year }
+  const hideCalendar = (): void  => {
+    setOpen(false)
   }
 
-  showCalendar (): void {
-    this.setState({ open: true })
+  const clearInput = (): void => {
+    onChange(null)
   }
 
-  hideCalendar (): void {
-    this.setState({ open: false })
+  const goToCurrentYear = (): void => {
+    setYear(currentYear)
   }
 
-  clearInput () {
-    this.props.onChange && this.props.onChange(null)
-  }
-
-  goToCurrentYear () {
-    this.setState({
-      // year: this.props.currentYear,
+  const goToNextYear = (): void => {
+    setYear((prevYear: number): number => {
+      if (max && prevYear >= max.year) return prevYear
+      return prevYear + 1
     })
   }
 
-  goToNextYear () {
-    this.setState((state: Readonly<MonthPickerState>): Pick<MonthPickerState, 'year'> | null => {
-      if (this.props.max && state.year >= this.props.max.year) return null
-      return {
-        year: state.year + 1,
-      }
+  const goToPrevYear = (): void => {
+    setYear((prevYear: number): number => {
+      if (min && prevYear <= min.year) return prevYear
+      return prevYear - 1
     })
   }
 
-  goToPrevYear () {
-    this.setState((state: Readonly<MonthPickerState>): Pick<MonthPickerState, 'year'> | null => {
-      if (this.props.min && state.year <= this.props.min.year) return null
-      return {
-        year: state.year - 1,
-      }
-    })
+  const monthClick = (month: number): void => {
+    hideCalendar()
+    onChange({ month, year })
   }
 
-  monthClick = (month: number): void => {
-    const { year } = this.state
-    this.hideCalendar()
-    this.props.onChange({ month, year })
-  }
-
-  // getChildContext () {
-  //   return { currentMonth: this.props.currentMonth }
-  // }
-
-  handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Escape') {
       event.preventDefault()
-      this.hideCalendar()
-      if (this.props.isClearable) {
-        this.clearInput()
+      hideCalendar()
+      if (isClearable) {
+        clearInput()
       }
     }
   }
 
-  onClearInputClick = (_: MouseEvent) => {
-    this.hideCalendar()
-    this.clearInput()
+  const onClearInputClick = (_: MouseEvent) => {
+    hideCalendar()
+    clearInput()
   }
 
-  override render () {
-    const { name, className, value, max, min } = this.props
-    const { year, open } = this.state
-    return (
-      <div className="monthpicker_container">
-        <input
-          type="hidden"
-          name={name}
-          value={value ? value.year + '-' + (value.month + 1) : ''} />
-        <input
-          type="text"
-          readOnly
-          value={value ? (value.month + 1) + '/' + value.year : ''}
-          onClick={this.showCalendar}
-          onFocus={this.showCalendar}
-          onBlur={this.hideCalendar}
-          onKeyDown={this.handleKeyDown}
-          className={className + ' monthpicker_input'} />
-        { this.props.isClearable && this.props.value ? <i className="glyphicon glyphicon-remove-circle monthpicker_clear_icon" onClick={this.onClearInputClick}></i> : null }
-        <div
-          className="monthpicker"
-          style={{ display: open ? 'block' : 'none' }}
-          onMouseDown={preventDefault}>
-          <table>
-            <Header
-              year={year}
-              goToCurrentYear={this.goToCurrentYear}
-              goToPrevYear={this.goToPrevYear}
-              goToNextYear={this.goToNextYear} />
-            <Months
-              min={min && year === min.year ? min.month : undefined}
-              max={max && year === max.year ? max.month : undefined}
-              selected={value ? value.month : null}
-              monthClick={this.monthClick}/>
-          </table>
-        </div>
+  return (
+    <div className="monthpicker_container">
+      <input
+        type="hidden"
+        name={name}
+        value={value ? value.year + '-' + (value.month + 1) : ''} />
+      <input
+        type="text"
+        readOnly
+        value={value ? (value.month + 1) + '/' + value.year : ''}
+        onClick={showCalendar}
+        onFocus={showCalendar}
+        onBlur={hideCalendar}
+        onKeyDown={handleKeyDown}
+        className={className + ' monthpicker_input'} />
+      { isClearable && value ? <i className="glyphicon glyphicon-remove-circle monthpicker_clear_icon" onClick={onClearInputClick}></i> : null }
+      <div
+        className="monthpicker"
+        style={{ display: open ? 'block' : 'none' }}
+        onMouseDown={preventDefault}>
+        <table>
+          <Header
+            year={year}
+            goToCurrentYear={goToCurrentYear}
+            goToPrevYear={goToPrevYear}
+            goToNextYear={goToNextYear} />
+          <Months
+            min={min && year === min.year ? min.month : undefined}
+            max={max && year === max.year ? max.month : undefined}
+            selected={value ? value.month : null}
+            monthClick={monthClick}/>
+        </table>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-const currentYear = (new Date()).getFullYear()
-const currentMonth = (new Date()).getMonth()
-const minMonth = { year: 1970, month: 0 }
-const maxMonth = { year: 1970, month: 0 }
-
-// MonthPicker.defaultProps = MonthPickerControlled.defaultProps = {
-//   isClearable: false,
-//   min: minMonth,
-//   max: maxMonth,
-//   currentYear,
-//   currentMonth,
-// }
-
-// MonthPickerControlled.childContextTypes = {
-//   currentMonth: PropTypes.number,
-// }
 
 function preventDefault (event: MouseEvent) {
   event.preventDefault()
-}
-
-export function parseYearMonth (value: string, min: YearMonth = minMonth, max: YearMonth = maxMonth) {
-  var split, year, month
-  value = String(value)
-  if (value.indexOf('.') !== -1) {
-    split = value.split('.')
-  } else if (value.indexOf('-') !== -1) {
-    split = value.split('-')
-  } else {
-    return {
-      year: currentYear,
-      month: currentMonth,
-    }
-  }
-
-  if (split[0] === undefined || split[1] === undefined) {
-    throw new Error('Invalid month');
-  }
-
-  year = parseInt(split[0], 10)
-  month = parseInt(split[1], 10)
-
-  // O mes na url é de 1-12, mas internamente é de 0-11.
-  month -= 1
-
-  year = Math.min(year, max.year)
-  year = Math.max(year, min.year)
-
-  month = Math.min(month, max.month, 11)
-  month = Math.max(month, min.month, 0)
-
-  return { year, month }
-}
-
-export function lastYearMonth (): YearMonth {
-  const now = new Date()
-  return {
-    year: now.getFullYear() - (now.getMonth() === 0 ? 1 : 0),
-    month: now.getMonth() === 0 ? 11 : (now.getMonth() - 1),
-  }
-}
-
-export function anoMesToUrl (anomes: YearMonth): string {
-  // O mes na url é de 1-12, mas internamente é de 0-11.
-  return `${anomes.year}-${anomes.month + 1}`
-}
-
-export function anoMesToDate (anomes: YearMonth): Date {
-  return new Date(`${anomes.year}-${anomes.month + 1}-1 00:00:00.000`)
 }
